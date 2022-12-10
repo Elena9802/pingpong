f7a38c40d23e1563e78382c5792fa04308d05911
Elena9802 <119110019+Elena9802@users.noreply.github.com>
2022-12-10 15:41:45 +0300

    ping

---
+++ ping.py
@@ -0,0 +1,58 @@
+from pygame import*
+import time as tm
+
+W_W = 700
+W_H = 500
+
+window = display.set_mode((W_W, W_H))
+window.fill(( 0, 0, 255))
+game = True
+clock = time.Clock()
+class Sprite(sprite.Sprite):
+    def __init__(self, fn, x, y, w, h, speedx, speedy):
+        self.image = image.load(fn)
+        self.image = transform.scale(self.image, (w, h))
+        self.rect = self.image.get_rect()
+        self.rect.x = x
+        self.rect.y = y
+        self.w = w
+        self.h = h
+        self.speedx = speedx
+        self.speedy = speedy
+    def update(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
+
+class Balls(Sprite):
+    def update(self):
+        self.rect.y += self.speedy
+        self.rect.x += self.speedx
+        if self.rect.y >= W_H or self.rect.y <= 0:
+            self.speedy *= -1
+        if self.rect.x >= W_W or self.rect.x <= 0:
+            self.speedx *= -1
+        super().update()
+
+class Player(Sprite):
+    def update(self):
+        key_pressed = key.get_pressed()
+        if key_pressed[K_UP] and self.rect.y + self.speedy > 0 :
+            self.rect.y -= self.speedy
+        elif key_pressed[K_DOWN] and self.rect.y - self.w - self.speedy < W_H:
+            self.rect.y += self.speedy
+        super().update()
+
+
+ball = Balls('лол.jpg', 0, 0, 30, 30, 3, 3)
+Gamer1 = Player('GOC.jpg', 40, 0, 20, 70, 4, 4)
+
+while game:
+    clock.tick(60)
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+
+    window.fill(( 0, 0, 255))
+    ball.update()
+    Gamer1.update()
+    
+    display.update()
